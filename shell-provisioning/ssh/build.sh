d2bc5a16a170caa8268a9cc7b49c9ee1ac779754
CJ Harries <cj@wizardsoftheweb.pro>
2017-10-22 14:24:58 -0500

    Refactor ssh directory for easier provisioning

--- shell-provisioning/ssh/build.sh
+++ shell-provisioning/ssh/build.sh
@@ -3,8 +3,8 @@
 # Storing passphrases in the clear (either in a script or in your history) is
 # never a good idea. Don't script generating your passphrase.
 
-rm -f ./*_rsa*
+rm -f ./keys/*_rsa*
 
 common_opts="-P password -b 2048 -t rsa"
 
-ssh-keygen $common_opts -f baseuser_generic_rsa -C baseuser@generic
+ssh-keygen $common_opts -f keys/baseuser_generic_rsa -C baseuser@generic
