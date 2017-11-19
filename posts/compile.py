6c7a36b07a6694bbfdf6f445ac7b6a0a05036953
CJ Harries <cj@wizardsoftheweb.pro>
2017-11-19 06:12:34 -0600

    Refactor to number duplicated headers

--- posts/compile.py
+++ posts/compile.py
@@ -16,14 +16,13 @@ rmtree(build_dir, ignore_errors=True)
 mkdir(build_dir)
 
 def run_bash(command_as_tuple):
-    readable_command = ' '.join(command_as_tuple)
-    output = "$ %(readable_command)s\n" % locals()
-    output += check_output(command_as_tuple)
-    return output
+    output = ('$ %s\n' % (' '.join(command_as_tuple))).encode('utf-8')
+    return ('%s%s' % (output, check_output(command_as_tuple))).decode('utf-8')
 
 def include_with_default(current_tag, relative_path, language):
     language = language or path.splitext(path.basename(relative_path))[1].replace('.', '')
     new_include = "```%(language)s\n" % locals()
+    new_include = new_include.replace('```yml', '```yaml')
     try:
         loaded_contents = check_output(
             ['git', 'show', current_tag + ':' + relative_path],
@@ -34,7 +33,20 @@ def include_with_default(current_tag, relative_path, language):
             loaded_contents = file_to_include.read()
     new_include += "%(loaded_contents)s\n" % locals()
     new_include += '```\n'
-    return new_include
+    return new_include.decode('utf-8')
+
+used_headlines = dict()
+def link_header(matched_line):
+    global used_headlines
+    headline = matched_line.group(2)
+    cleaned_headline = sub('[^a-z]', '', headline.lower())
+    if cleaned_headline in used_headlines:
+        used_headlines[cleaned_headline] += 1
+        cleaned_headline += str(used_headlines[cleaned_headline])
+    else:
+        used_headlines[cleaned_headline] = 0
+    return "%s[%s](#%s)%s" % (matched_line.group(1), headline, cleaned_headline, matched_line.group(3))
+
 
 jinja_env = Environment(
     loader=FileSystemLoader( template_dir )
@@ -45,6 +57,7 @@ jinja_env.globals['num2words'] = num2words
 jinja_env.globals['run_bash'] = run_bash
 
 for post_filename in glob(path.join(template_dir, 'post-*.j2')):
+    used_headlines = dict()
     post_basename = path.basename(post_filename)
     template = jinja_env.get_template(post_basename)
     post_basename = post_basename.replace('.j2', '')
@@ -54,7 +67,7 @@ for post_filename in glob(path.join(template_dir, 'post-*.j2')):
     )
     output_filename = path.join(build_dir, post_basename.replace('.j2', ''))
     with file(output_filename, 'w+') as built_file:
-        built_file.write(sub(r'\n\n```\n\n', '\n```\n\n', pre_toc))
+        built_file.write(sub(r'\n\n```\n\n', '\n```\n\n', pre_toc.encode('utf-8')))
         built_file.seek(0)
         built_file.write(
             sub(
@@ -75,12 +88,6 @@ for post_filename in glob(path.join(template_dir, 'post-*.j2')):
         full_toc = search(r'<p class="nav-p"><a id="post-nav"></a></p>\s+(-.*?)\n\r?\n\r?', file_contents, DOTALL)
         if full_toc:
             new_toc = full_toc.group()
-            for dashed_slug in finditer(r'\s*-\s+([^\n]+)', new_toc):
-                headline = dashed_slug.group(1)
-                cleaned_headline = sub('[^a-z]', '', headline.lower())
-                new_toc = new_toc.replace(
-                    dashed_slug.group(1),
-                    "[%(headline)s](#%(cleaned_headline)s)" % locals()
-                )
+            new_toc = sub(r'(\s*-\s+)([^\n]+)(\n)', link_header, new_toc)
             built_file.seek(0)
             built_file.write(file_contents.replace(full_toc.group(), new_toc))
